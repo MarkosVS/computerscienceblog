172366b5ada5b459e3d546148de0cfc4b954c80e
Marcos Sombra <markos.vs@live.com>
2017-07-05 06:56:38 -0300

    adicionando corpo do site

--- index.php
+++ index.php
@@ -19,6 +19,17 @@
 
 		<!-- inicio section corpo -->
 		<section id="corpo">
+			<div id="corpoMain">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque cursus justo sem, a hendrerit purus volutpat vel. Sed sit amet hendrerit arcu. Sed et facilisis purus. Aenean sagittis volutpat quam eu eleifend. Ut sed lorem sem. Quisque efficitur eros sit amet nibh molestie efficitur. Mauris tellus odio, ultrices iaculis ante nec, interdum tempus magna. Donec vel nulla aliquet, porta neque ac, accumsan tellus. Nulla ac nisi nulla. Nullam id risus id quam tincidunt tempus. Fusce faucibus odio et urna ultricies sodales. Aliquam et sagittis libero. Suspendisse blandit ullamcorper pellentesque. Vivamus ut leo sapien. Duis quis egestas est. Quisque varius consequat ipsum, blandit placerat diam.
+
+				Integer at sapien eu massa placerat congue ac et mi. Ut luctus massa metus, ac dignissim nisl suscipit vel. Fusce interdum sagittis dapibus. Nullam convallis scelerisque arcu. Nullam rutrum metus leo, sed dignissim mi tempus nec. Nunc facilisis, massa vel finibus luctus, nunc arcu semper odio, et ullamcorper massa purus id ante. Integer pretium non enim non viverra. Etiam at dictum augue. Nam pellentesque ullamcorper lectus, non iaculis libero egestas eu. Sed velit dolor, suscipit lacinia nisl sit amet, mattis maximus velit. Maecenas ullamcorper sit amet orci in fermentum. Cras interdum neque id nibh suscipit malesuada. Etiam vitae urna in ex vehicula lacinia placerat quis nisl. Donec eros est, rutrum ut dolor nec, consectetur consequat turpis. Phasellus vitae tellus tortor.
+
+				Etiam placerat dolor libero, id malesuada turpis rhoncus non. Integer quis rutrum odio. Morbi viverra, nisi eu finibus blandit, massa eros pellentesque ligula, sit amet bibendum dolor neque vitae nisl. Praesent et ante risus. Donec a rhoncus ex, vel porta lorem. Proin placerat mi est, quis pretium ipsum faucibus et. Maecenas nec massa nisl. Nulla pellentesque finibus dapibus.
+
+				Morbi tristique ultricies dolor eu dictum. Integer id finibus urna, in ultricies ligula. Morbi lobortis aliquam sapien, ac ullamcorper purus bibendum eu. Pellentesque quis justo a diam dapibus maximus. Duis porttitor euismod enim, ac rutrum nunc egestas sit amet. Nunc nec nisl in ante ornare tincidunt. Praesent blandit diam et nisi malesuada fringilla.
+
+				Nullam porta lectus ut est rutrum pulvinar. Curabitur vestibulum ac tellus vitae fermentum. Cras dictum ut eros vitae dictum. Cras eget tortor quis nisi luctus placerat quis ut eros. Aenean lobortis sit amet odio vitae molestie. Morbi quis justo eu nisi rhoncus consequat. Sed dapibus nec eros mattis auctor. Sed vel mattis sem, quis molestie ligula. Aenean ornare vestibulum magna, vel euismod purus.
+			</div>
 		</section>
 		<!-- fim section corpo -->
 
