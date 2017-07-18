25cecb77a63070ad1a598d7b926f1072a1cfc35f
Marcos Sombra <markos.vs@live.com>
2017-07-18 15:53:53 -0300

    add icone inicial

--- includes/head.php
+++ includes/head.php
@@ -3,6 +3,9 @@
 <html lang = "pt-br">
 	<!-- inicio head -->
 	<head>
-		<link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">  <!-- linka o arquivo da fonte Oswald-->
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald">  <!-- linka o arquivo da fonte Oswald-->
 		<link rel="stylesheet" type="text/css" href="css/style.css"> <!-- linka o arquivo style.css -->
-		<meta charset="utf-8">
+        <link rel="icon" href="icon.jpg"> <!-- linka o icone icon.jpg-->
+		
+        <meta charset="utf-8"> <!-- seta charset como utf-8-->
+        <meta name="author" content="Marcos Vinicius Sombra & Beatriz Vasconcelos"> <!-- informa os autores do site -->
