0c109ea00157da8b0a5b7fb3ad77ef9ffbf9c1eb
Marcos Sombra <markos.vs@live.com>
2017-07-23 13:53:47 -0300

    add página projetos

--- includes/menu.php
+++ includes/menu.php
@@ -1,7 +1,7 @@
 <nav id="navMenu">
 			<ul id="listMenu">
 				<li><a href="index.php">Home</a></li>
-				<li><a href="#">Projetos</a></li>
+				<li><a href="projetos.php">Projetos</a></li>
 				<li><a href="sobre.php">Sobre</a></li>
 			</ul>
 		</nav>
