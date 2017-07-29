24af72f948d49508d93f87f2c59374e7357f7e7a
Marcos Sombra <markos.vs@live.com>
2017-07-29 16:30:23 -0300

    corrigindo menu

--- includes/menu.php
+++ includes/menu.php
@@ -3,6 +3,6 @@
 				<li><a href="index.php">Home</a></li>
 				<li><a href="projetos.php">Projetos</a></li>
 				<li><a href="sobre.php">Sobre</a></li>
-                <li><a href="login.php">Sobre</a></li>
+                <li><a href="login.php">Entrar / Registrar</a></li>
 			</ul>
 		</nav>
