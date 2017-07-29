0ea3e93dbcd1017438a42cf5b717a13103c3d6f4
Marcos Sombra <markos.vs@live.com>
2017-07-29 19:00:51 -0300

    add descriçao inicial

--- sobre.php
+++ sobre.php
@@ -20,7 +20,14 @@
 		<!-- inicio section corpo -->
 		<section id="corpo">
 			<div id="corpoMain">
-                                
+                <h2>Sobre Sombia:</h2>
+                <p>Beatriz Vasconcelos e Marcos Vinicius Sombra são um casal de namorados e alunos do 3º semestre do curso de Ciência da Computação da Universidade Estadual do Ceará (UECE). Juntos, criaram este blog (totalmente desenvolvido pelos mesmos) onde contam seu dia a dia como casal e como estudantes de Computação.</p>
+                
+                <p><strong>Bia</strong> atualmente é bolsista do PET-Computação da UECE e já foi monitora da disciplina de Matemática Discreta. Batman nas horas vagas.<br>
+                <a class="socialmedia" href="https://www.facebook.com/biavasconceloss" target="_blank">Facebook</a></p>
+                
+                <p><strong>Marcos</strong> atualmente é membro do CAcomp UECE e já foi Trainee na Acens.<br>
+                <a class="socialmedia" href="https://www.facebook.com/maarkinvinicius" target="_blank">Facebook</a> || <a class="socialmedia" href="https://www.github.com/MarkosVS" target="_blank">Github</a></p>
 			</div>
 		</section>
 		<!-- fim section corpo -->
