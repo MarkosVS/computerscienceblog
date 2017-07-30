b2e586770d62cac4b74647aa4f1920e0b14fbd1e
Marcos Sombra <markos.vs@live.com>
2017-07-30 17:13:22 -0300

    removendo reset e adicionando bootsrap

--- includes/head.php
+++ includes/head.php
@@ -8,7 +8,8 @@
 	<!-- inicio head -->
 	<head>
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald">  <!-- linka o arquivo da fonte Oswald-->
-		<link rel="stylesheet" type="text/css" href="css/reset.css"> <!-- linka o arquivo reset.css -->
+        
+        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css"> <!-- linka bootstrap -->  
         <link rel="stylesheet" type="text/css" href="css/style.css"> <!-- linka o arquivo style.css -->
         
         <link rel="icon" href="icon.jpg"> <!-- linka o icone icon.jpg-->
