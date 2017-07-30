8265d9794ed9b2ff959db61a00d9ca8a954581d2
Marcos Sombra <markos.vs@live.com>
2017-07-30 17:44:13 -0300

    add imagens nas descrições

--- sobre.php
+++ sobre.php
@@ -21,11 +21,13 @@
 		<section id="corpo">
 			<div id="corpoMain">
                 <h2>Sobre Sombia:</h2>
-                <p>Beatriz Vasconcelos e Marcos Vinicius Sombra são um casal de namorados e alunos do 3º semestre do curso de Ciência da Computação da Universidade Estadual do Ceará (UECE). Juntos, criaram este blog (totalmente desenvolvido pelos mesmos) onde contam seu dia a dia como casal e como estudantes de Computação.</p>
+                <p>Beatriz Vasconcelos e Marcos Vinicius Sombra são um casal de namorados e alunos do 3º semestre do curso de Ciência da Computação da Universidade Estadual do Ceará (UECE). Juntos, criaram este blog (totalmente desenvolvido pelos mesmos) onde contam seu dia a dia como casal e como estudantes de Computação.</p><br>
                 
-                <p><strong>Bia</strong> atualmente é bolsista do PET-Computação da UECE e já foi monitora da disciplina de Matemática Discreta. Batman nas horas vagas. Também é conhecida como mulher mais linda do mundo.<br>
-                <a class="socialmedia" href="https://www.facebook.com/biavasconceloss" target="_blank">Facebook</a></p>
+                <img src="img/bia.jpg" title="Bia Vasconcelos" alt="Bia Vasconcelos" class="img-desc">
+                <p><strong>Bia</strong> atualmente é bolsista do PET-Computação da UECE e já foi monitora da disciplina de Matemática Discreta. Batman nas horas vagas. Ama moda/maquiagem e solar todo mundo no lol. Também é conhecida como mulher mais linda do mundo.<br>
+                <a class="socialmedia" href="https://www.facebook.com/biavasconceloss" target="_blank">Facebook</a> || <a class="socialmedia" href="https://www.github.com/QueenBia" target="_blank">Github</a></p>
                 
+                <img src="img/sombra.jpg" title="Marcos Sombra" alt="Marcos Sombra" class="img-desc">
                 <p><strong>Marcos</strong> atualmente é membro do CAcomp UECE e já foi Trainee na Acens. Amante da mulher mais linda do mundo. Apaixonado por Harry Potter, Java, café e código. Ás vezes, entusiasta de divulgação científica.<br>
                 <a class="socialmedia" href="https://www.facebook.com/maarkinvinicius" target="_blank">Facebook</a> || <a class="socialmedia" href="https://www.github.com/MarkosVS" target="_blank">Github</a></p>
 			</div>
