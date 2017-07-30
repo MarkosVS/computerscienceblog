25f6ef1e6aaabfabc68d366882d8fdcc095a4212
Marcos Sombra <markos.vs@live.com>
2017-07-30 16:50:24 -0300

    add css reset

--- includes/head.php
+++ includes/head.php
@@ -8,7 +8,9 @@
 	<!-- inicio head -->
 	<head>
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald">  <!-- linka o arquivo da fonte Oswald-->
-		<link rel="stylesheet" type="text/css" href="css/style.css"> <!-- linka o arquivo style.css -->
+		<link rel="stylesheet" type="text/css" href="css/reset.css"> <!-- linka o arquivo reset.css -->
+        <link rel="stylesheet" type="text/css" href="css/style.css"> <!-- linka o arquivo style.css -->
+        
         <link rel="icon" href="icon.jpg"> <!-- linka o icone icon.jpg-->
 		
         <meta charset="utf-8"> <!-- seta charset como utf-8-->
