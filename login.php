cb3d49428330bb2283416214c3a026801e90859b
Marcos Sombra <markos.vs@live.com>
2017-08-01 20:36:53 -0300

    criando sistema de login (beta)

--- login.php
+++ login.php
@@ -20,31 +20,61 @@
 		<!-- inicio section corpo -->
 		<section id="corpo">
 			<div id="corpoMain">
-                <form class="form-horizontal" id="form-login" method="post">
+                <form class="form-horizontal" id="form-login" method="post" action="#">
                     <h2>Entrar</h2><br>
                     <div class="form-group">
                         <label for="inputEmail3" class="col-sm-2 control-label">E-mail</label>
                         <div class="col-sm-10">
-                            <input type="email" class="form-control" id="inputEmail3" placeholder="E-mail" required>
+                            <input type="email" class="form-control" id="inputEmail3" placeholder="E-mail" name="email" required>
                         </div>
                     </div>
 
                     <div class="form-group">
                         <label for="inputPassword3" class="col-sm-2 control-label">Senha</label>
                         <div class="col-sm-10">
-                            <input type="password" class="form-control" id="inputPassword3" placeholder="Senha" required>
+                            <input type="password" class="form-control" id="inputPassword3" placeholder="Senha" name="senha" required>
                         </div>
                     </div>
 
                     <div class="form-group">
                         <div class="col-sm-offset-2 col-sm-10">
-                            <button type="submit" class="btn btn-default">Entrar</button>
+                            <button type="submit" class="btn btn-default" name="entrar">Entrar</button>
                         </div>
                     </div>
+                    
+                    <?PHP
+                    //realizar login no site
+                    if(isset($_POST['entrar'])){
+                        $email = trim(strip_tags($_POST['email']));
+                        $senha = trim(strip_tags($_POST['senha']));
+                        
+                        $select = "SELECT * from usuarios WHERE BINARY email=:email AND BINARY senha=:senha";
+                        try{
+                            $result = $conexao->prepare($select);
+                            $result->bindParam(':email', $email, PDO::PARAM_STR);
+                            $result->bindParam(':senha', $senha, PDO::PARAM_STR);
+                            $result->execute();
+                            $c = $result->rowCount();
+                            if($c > 0){
+                                $email = $_POST['email'];
+								$senha = $_POST['senha'];
+                                $_SESSION['emailsombia'] = $email;
+                                $_SESSION['senhasombia'] = $senha;
+                                echo 'Conectado com sucesso.';
+                                header("Refresh: 1, index.php");
+                            }else{
+								echo 'Email ou senha incorretos';
+							}
+                        }catch(PDOException $erro){
+                            echo 'ERROR: '.$erro;
+                        }
+                    }
+                ?>
+                    
                 </form>
                 
                 <br>
-                <form class="form-horizontal" id="form-login" method="post">
+                <form class="form-horizontal" id="form-login" method="post" action="#">
                     <h2>Registrar</h2><br>
                     <div class="form-group">
                         <label for="inputEmail3" class="col-sm-2 control-label">Nome</label>
