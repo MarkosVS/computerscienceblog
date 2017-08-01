55c80447bbbe7caef182da933a4ff32f5ea68641
Marcos Sombra <markos.vs@live.com>
2017-08-01 10:14:04 -0300

    add form registro

--- login.php
+++ login.php
@@ -41,7 +41,45 @@
                             <button type="submit" class="btn btn-default">Entrar</button>
                         </div>
                     </div>
-                </form>            
+                </form>
+                
+                <br>
+                <form class="form-horizontal" id="form-login" method="post">
+                    <h2>Registrar</h2><br>
+                    <div class="form-group">
+                        <label for="inputEmail3" class="col-sm-2 control-label">Nome</label>
+                        <div class="col-sm-10">
+                            <input type="email" class="form-control" id="inputEmail3" placeholder="Nome" required>
+                        </div>
+                    </div>
+                    
+                    <div class="form-group">
+                        <label for="inputEmail3" class="col-sm-2 control-label">E-mail</label>
+                        <div class="col-sm-10">
+                            <input type="email" class="form-control" id="inputEmail3" placeholder="E-mail" required>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="inputPassword3" class="col-sm-2 control-label">Senha</label>
+                        <div class="col-sm-10">
+                            <input type="password" class="form-control" id="inputPassword3" placeholder="Senha" required>
+                        </div>
+                    </div>
+                    
+                    <div class="form-group">
+                        <label for="inputPassword3" class="col-sm-2 control-label">Senha</label>
+                        <div class="col-sm-10">
+                            <input type="password" class="form-control" id="inputPassword3" placeholder="Senha" required>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-10">
+                            <button type="submit" class="btn btn-default">Registrar</button>
+                        </div>
+                    </div>
+                </form> 
 			</div>
 		</section>
 		<!-- fim section corpo -->
