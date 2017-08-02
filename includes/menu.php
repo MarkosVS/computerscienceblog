5da88363b3d2608718b82e3c3a989a5b09776b37
Marcos Sombra <markos.vs@live.com>
2017-08-02 17:56:19 -0300

    add página de posts e menu do painel de controle

--- includes/menu.php
+++ includes/menu.php
@@ -1,7 +1,7 @@
 <nav id="navMenu">
 			<ul id="listMenu">
 				<li><a href="index.php">Home</a></li>
-				<li><a href="projetos.php">Projetos</a></li>
+				<li><a href="posts.php">Posts</a></li>
 				<li><a href="sobre.php">Sobre</a></li>
                 
                 <?PHP
@@ -11,7 +11,11 @@
                 <?PHP
                     }
                     if(isset($_SESSION['emailsombia']) && (isset($_SESSION['senhasombia']))){ //só aparecem esses menus se houver uma session ativa
-                    
+                        if($nivelLog == 10 || $nivelLog == 9){
+                ?> 
+                            <li><a href="#">Painel de Controle</a></li>
+                <?PHP
+                        }
                 ?>
                         <li><a href="?sair"><?PHP echo $nomeLog;?> (Sair)</a></li>
                 <?PHP
---
+++ posts.php
@@ -0,0 +1,64 @@
+<?php
+	include("includes/head.php"); //inclui o head quase completo do site
+?>
+		<title>Posts - Sombia Development</title> <!-- muda o título do site -->
+	</head>
+	<!-- fim head -->
+
+	<!-- inicio body -->
+	<body>
+		<?php
+			include("includes/header.php"); //inclui o cabeçalho do site
+		?>
+
+		<!-- inicio nav navMenu -->
+		<?php
+			include("includes/menu.php"); //inclui o menu do site
+		?>
+		<!-- fim nav navMenu -->
+
+		<!-- inicio section corpo -->
+		<section id="corpo">
+			<div id="corpoMain">                
+                <div id="posts">
+                    <?PHP 
+                        $sql = "SELECT * from posts WHERE exibir = 'Sim' ORDER BY id DESC LIMIT 15";
+                        try{
+                            $resultado = $conexao->prepare($sql);
+                            $resultado->execute();
+                            $cont = $resultado->rowCount();
+                            if($cont > 0){
+                                while($mostrar = $resultado->fetch(PDO::FETCH_OBJ)){
+                                    ?>
+                                    <div class="post">
+                                        <h3><?PHP echo $mostrar->titulo ?></h3>
+                                        <img alt="<?PHP echo $mostrar->titulo ?>" title="<?PHP echo $mostrar->titulo ?>" src="img/<?PHP echo $mostrar->img ?>">
+                                        <p><?PHP echo $mostrar->texto ?> </p>
+
+                                        <p>Publicado por <?PHP echo $mostrar->autor ?> em <?PHP echo $mostrar->data ?></p>
+                                    </div>
+                                    <?PHP
+                                }
+                            }else{
+                                echo '<h2>Bem-vindos ao Blog Sombia :)</h2>';
+                            }
+                        }catch(PDOException $erro){
+                            echo 'ERROR: '.$erro;
+                        }
+                    ?>
+                    
+                    
+                    
+                </div>  
+			</div>
+		</section>
+		<!-- fim section corpo -->
+
+		<!-- inicio footer -->
+		<?php
+			include("includes/footer.php"); //inclui o rodapé do site
+		?>
+		<!-- fim footer -->
+	</body>
+	<!-- inicio body -->
+</html>
