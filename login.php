2fd3f5553cac8e1a3445bbe2f122c3ab897de1a9
Marcos Sombra <markos.vs@live.com>
2017-08-01 21:23:36 -0300

    add criptografia basica e criando sistema de cadastro

--- login.php
+++ login.php
@@ -46,7 +46,7 @@
                     //realizar login no site
                     if(isset($_POST['entrar'])){
                         $email = trim(strip_tags($_POST['email']));
-                        $senha = trim(strip_tags($_POST['senha']));
+                        $senha = md5(trim(strip_tags($_POST['senha'])));
                         
                         $select = "SELECT * from usuarios WHERE BINARY email=:email AND BINARY senha=:senha";
                         try{
@@ -69,7 +69,7 @@
                             echo 'ERROR: '.$erro;
                         }
                     }
-                ?>
+                    ?>
                     
                 </form>
                 
@@ -79,36 +79,82 @@
                     <div class="form-group">
                         <label for="inputEmail3" class="col-sm-2 control-label">Nome</label>
                         <div class="col-sm-10">
-                            <input type="email" class="form-control" id="inputEmail3" placeholder="Nome" required>
+                            <input type="text" class="form-control" id="inputNome3" placeholder="Nome" name="nome" required>
                         </div>
                     </div>
                     
                     <div class="form-group">
                         <label for="inputEmail3" class="col-sm-2 control-label">E-mail</label>
                         <div class="col-sm-10">
-                            <input type="email" class="form-control" id="inputEmail3" placeholder="E-mail" required>
+                            <input type="email" class="form-control" id="inputEmail3" placeholder="E-mail" name="email" required>
                         </div>
                     </div>
 
                     <div class="form-group">
                         <label for="inputPassword3" class="col-sm-2 control-label">Senha</label>
                         <div class="col-sm-10">
-                            <input type="password" class="form-control" id="inputPassword3" placeholder="Senha" required>
+                            <input type="password" class="form-control" id="inputPassword3" placeholder="Senha" name="senha" required>
                         </div>
                     </div>
                     
                     <div class="form-group">
                         <label for="inputPassword3" class="col-sm-2 control-label">Senha</label>
                         <div class="col-sm-10">
-                            <input type="password" class="form-control" id="inputPassword3" placeholder="Senha" required>
+                            <input type="password" class="form-control" id="inputPassword30" placeholder="Senha" name="csenha" required>
                         </div>
                     </div>
 
                     <div class="form-group">
                         <div class="col-sm-offset-2 col-sm-10">
-                            <button type="submit" class="btn btn-default">Registrar</button>
+                            <button type="submit" class="btn btn-default" name="registrar">Registrar</button>
                         </div>
                     </div>
+                    
+                    <?PHP
+                    //realizar login no site
+                    if(isset($_POST['registrar'])){
+                        $regNome = trim(strip_tags($_POST['nome']));
+                        $regEmail = trim(strip_tags($_POST['email']));
+                        $regSenha = md5(trim(strip_tags($_POST['senha'])));
+                        $regConf = md5(trim(strip_tags($_POST['csenha'])));
+                        $regNivel = 1;
+                        
+                        if($regSenha == $regConf){
+                            $busca = "SELECT * from usuarios WHERE email LIKE '$regEmail'";
+                            $result = $conexao->prepare($busca);
+                            $result->execute();
+                            $cont = $result->rowCount();
+                            
+                            if($cont > 0){
+                                echo "E-mail já cadastrado";
+                            }else{
+                                $insert = "INSERT into usuarios (nome, email, senha, nivel) VALUES (:regNome, :regEmail, :regSenha, $regNivel)";
+                                
+                                try{
+                                    $resultado = $conexao->prepare($insert);
+                                    $resultado->bindParam(':regNome', $regNome, PDO::PARAM_STR);
+                                    $resultado->bindParam(':regEmail', $regEmail, PDO::PARAM_STR);
+                                    $resultado->bindParam(':regSenha', $regSenha, PDO::PARAM_STR);
+                                    $resultado->execute();
+                                    
+                                    $contar = $resultado->rowCount();
+                                    if($contar > 0){
+                                        echo 'Usuário cadastrado com sucesso';
+                                    }else{
+                                        echo 'Não foi possível cadastrar o usuário';
+                                    }
+                                    
+                                }catch(PDOException $erro){
+                                    echo 'ERROR: '.$erro;
+                                }
+                            }
+                            
+                        }else{
+                            echo 'As senhas não correspondem';
+                        }
+                    }
+                    ?>
+                    
                 </form> 
 			</div>
 		</section>
