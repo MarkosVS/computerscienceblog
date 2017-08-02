190ddf3369ddc5de4341fa6a7fab7339c3429b28
Marcos Sombra <markos.vs@live.com>
2017-08-01 21:29:24 -0300

    att

--- login.php
+++ login.php
@@ -100,7 +100,7 @@
                     <div class="form-group">
                         <label for="inputPassword3" class="col-sm-2 control-label">Senha</label>
                         <div class="col-sm-10">
-                            <input type="password" class="form-control" id="inputPassword30" placeholder="Senha" name="csenha" required>
+                            <input type="password" class="form-control" id="inputPassword3" placeholder="Senha" name="csenha" required>
                         </div>
                     </div>
 
